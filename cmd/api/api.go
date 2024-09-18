20d1ccc83da34451c58e9dd24c8aaf07d7ffa0d0
AhmedMohamedAR <ahmed.mohamed@elevate-pay.com>
2024-09-18 19:48:29 +0300

    Initial commit

---
+++ cmd/api/api.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+type application struct {
+	config config
+}
+
+type config struct {
+	addr string
+}
+
+func (app *application) mount() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /v1/health", app.healthHandler)
+
+	return mux
+}
+
+func (app *application) run(mux *http.ServeMux) error {
+	srv := http.Server{
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: 30 * time.Second,
+		ReadTimeout:  10 * time.Second,
+		IdleTimeout:  time.Minute,
+	}
+
+	log.Printf("listening on %s", app.config.addr)
+	return srv.ListenAndServe()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AhmedARMohamed/social
+
+go 1.23.1
