f089ef074a96eab482fc397ed0384a8339d87cb1
AhmedMohamedAR <ahmed.mohamed@elevate-pay.com>
2024-10-10 18:30:36 +0300

    implement repository pattern and sql migrations

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,17 +1,39 @@
 package main
 
 import (
+	"log"
+
+	"github.com/AhmedARMohamed/social/internal/db"
 	"github.com/AhmedARMohamed/social/internal/env"
 	"github.com/AhmedARMohamed/social/internal/store"
-	"log"
 )
 
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://admin:admin@localhost:5435/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
 
-	store := store.NewStorage(nil)
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer db.Close()
+	log.Println("database connection pool established")
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
