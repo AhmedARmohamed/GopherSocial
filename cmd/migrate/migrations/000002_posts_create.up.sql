6cb7d60813f76ca1d7964afda750a1c6cddd4fa5
AhmedMohamedAR <ahmed.mohamed@elevate-pay.com>
2024-11-18 17:19:30 +0300

    Creating a user feed post

--- cmd/migrate/migrations/000002_posts_create.up.sql
+++ cmd/migrate/migrations/000002_posts_create.up.sql
@@ -2,5 +2,8 @@ CREATE TABLE IF NOT EXISTS posts (
     id bigserial PRIMARY KEY,
     title text NOT NULL,
     user_id bigint NOT NULL,
-    created_at TIMESTAMP(0) with time zone NOT NULL DEFAULT NOW()
+    content text NOT NULL,
+    tags VARCHAR(100) [] NOT NULL,
+    created_at TIMESTAMP(0) with time zone NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP(0) with time zone NOT NULL DEFAULT NOW()
 )
