7bc2189e7cf74a1cfb01ca4ea217a12ac14867b6
AhmedMohamedAR <ahmed.mohamed@elevate-pay.com>
2024-11-20 17:41:16 +0300

    implement internal errors package

---
+++ cmd/api/errors.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func (app *application) internalServerError(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("Internal Server error:  %s path %s error %s", r.Method, r.URL, err)
+	writeJSON(w, http.StatusInternalServerError, "The server encountered an internal error")
+
+}
+
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("bad request error:  %s path %s error %s", r.Method, r.URL, err)
+	writeJSON(w, http.StatusInternalServerError, err.Error())
+
+}
+
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("not found error %s path %s error %s", r.Method, r.URL, err)
+	writeJSON(w, http.StatusInternalServerError, "not found error")
+
+}
--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -19,7 +19,7 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 
 	var payload CreatePostPayload
 	if err := readJSON(w, r, &payload); err != nil {
-		writeJSONError(w, http.StatusBadRequest, err.Error())
+		app.badRequestResponse(w, r, err)
 		return
 	}
 
@@ -34,12 +34,12 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 	ctx := r.Context()
 
 	if err := app.store.Posts.Create(ctx, post); err != nil {
-		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		app.internalServerError(w, r, err)
 		return
 	}
 
 	if err := writeJSON(w, http.StatusCreated, post); err != nil {
-		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		app.internalServerError(w, r, err)
 		return
 	}
 }
@@ -50,7 +50,7 @@ func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
 
 	id, err := strconv.ParseInt(idParam, 10, 64)
 	if err != nil {
-		writeJSONError(w, http.StatusBadRequest, err.Error())
+		app.internalServerError(w, r, err)
 		return
 	}
 
@@ -58,15 +58,15 @@ func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		switch {
 		case errors.Is(err, store.ErrNotFound):
-			writeJSONError(w, http.StatusNotFound, err.Error())
+			app.notFoundResponse(w, r, err)
 		default:
-			writeJSONError(w, http.StatusInternalServerError, err.Error())
+			app.internalServerError(w, r, err)
 		}
 		return
 	}
 
 	if err := writeJSON(w, http.StatusOK, posts); err != nil {
-		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		app.internalServerError(w, r, err)
 		return
 	}
 }
@@ -76,12 +76,12 @@ func (app *application) listPostHandler(w http.ResponseWriter, r *http.Request) 
 
 	posts, err := app.store.Posts.List(ctx)
 	if err != nil {
-		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		app.internalServerError(w, r, err)
 		return
 	}
 
 	if err := writeJSON(w, http.StatusOK, posts); err != nil {
-		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		app.internalServerError(w, r, err)
 		return
 	}
 }
