ee132af8838d07b1c2517218f2b66a3483a4c36b
AhmedMohamedAR <ahmed.mohamed@elevate-pay.com>
2024-12-05 12:29:54 +0300

    optimistic concurrency control

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -16,6 +16,7 @@ type Post struct {
 	Tags      []string  `json:"tags"`
 	Comments  []Comment `json:"comments"`
 	CreatedAt string    `json:"created_at"`
+	Version   int       `json:"version"`
 	UpdatedAt string    `json:"updated_at"`
 }
 
@@ -24,7 +25,10 @@ type PostStore struct {
 }
 
 func (s *PostStore) Create(ctx context.Context, post *Post) error {
-	query := `INSERT INTO posts (content, title, user_id, tags) VALUES ($1, $2, $3, $4) RETURNING id, created_at, updated_at`
+	query := `
+INSERT INTO posts (content, title, user_id, tags)
+VALUES ($1, $2, $3, $4) RETURNING id, created_at, updated_at
+`
 	err := s.db.QueryRowContext(ctx, query, post.Content, post.Title, post.UserID, pq.Array(post.Tags)).Scan(&post.ID, &post.CreatedAt, &post.UpdatedAt)
 	if err != nil {
 		return err
@@ -33,10 +37,10 @@ func (s *PostStore) Create(ctx context.Context, post *Post) error {
 }
 
 func (s *PostStore) GetByID(ctx context.Context, id int64) (*Post, error) {
-	query := `SELECT id, content, title, user_id, tags, created_at, updated_at FROM posts WHERE id = $1`
+	query := `SELECT id, content, title, user_id, tags, version, created_at, updated_at FROM posts WHERE id = $1`
 
 	var post Post
-	err := s.db.QueryRowContext(ctx, query, id).Scan(&post.ID, &post.Content, &post.Title, &post.UserID, pq.Array(&post.Tags), &post.CreatedAt, &post.UpdatedAt)
+	err := s.db.QueryRowContext(ctx, query, id).Scan(&post.ID, &post.Content, &post.Title, &post.UserID, pq.Array(&post.Tags), &post.Version, &post.CreatedAt, &post.UpdatedAt)
 	if err != nil {
 		switch {
 		case errors.Is(err, sql.ErrNoRows):
@@ -49,7 +53,7 @@ func (s *PostStore) GetByID(ctx context.Context, id int64) (*Post, error) {
 }
 
 func (s *PostStore) List(ctx context.Context) ([]*Post, error) {
-	query := `SELECT id, content, title, user_id, tags, created_at, updated_at FROM posts`
+	query := `SELECT id, content, title, user_id, tags, version, created_at, updated_at FROM posts`
 
 	var posts []*Post
 	rows, err := s.db.QueryContext(ctx, query)
@@ -60,7 +64,7 @@ func (s *PostStore) List(ctx context.Context) ([]*Post, error) {
 
 	for rows.Next() {
 		var post Post
-		if err := rows.Scan(&post.ID, &post.Content, &post.Title, &post.UserID, pq.Array(&post.Tags), &post.CreatedAt, &post.UpdatedAt); err != nil {
+		if err := rows.Scan(&post.ID, &post.Content, &post.Title, &post.UserID, pq.Array(&post.Tags), &post.Version, &post.CreatedAt, &post.UpdatedAt); err != nil {
 			return nil, err
 		}
 		posts = append(posts, &post)
@@ -75,7 +79,10 @@ func (s *PostStore) List(ctx context.Context) ([]*Post, error) {
 }
 
 func (s *PostStore) Delete(ctx context.Context, id int64) error {
-	query := `DELETE FROM posts WHERE id = $1`
+	query := `
+DELETE FROM posts
+WHERE id = $1
+`
 
 	res, err := s.db.ExecContext(ctx, query, id)
 	if err != nil {
@@ -94,11 +101,28 @@ func (s *PostStore) Delete(ctx context.Context, id int64) error {
 }
 
 func (s *PostStore) Update(ctx context.Context, post *Post) error {
-	query := `UPDATE posts SET title = $1, content = $2 WHERE id = $3`
+	query := `
+UPDATE posts
+SET title = $1, content = $2, version = version + 1
+WHERE id = $3 AND version = $4
+RETURNING version
+`
 
-	_, err := s.db.ExecContext(ctx, query, post.Title, post.Content, post.ID)
+	err := s.db.QueryRowContext(
+		ctx,
+		query,
+		post.Title,
+		post.Content,
+		post.ID,
+		post.Version,
+	).Scan(&post.Version)
 	if err != nil {
-		return err
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrNotFound
+		default:
+			return err
+		}
 	}
 
 	return nil
